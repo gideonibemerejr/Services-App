7f1411632f563a58dab087965ca5e68fbf3aaebc
Gideon Ibemere Jr <goij@goij-pro.local>
2019-05-28 13:30:43 -0500

    Update: Complete Admin Service Views, Add Appt Calendar, Add User Profile

--- config/passport.js
+++ config/passport.js
@@ -4,57 +4,68 @@ const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/user');
 
-passport.use(new LocalStrategy(
+passport.use(
+  new LocalStrategy(
     {
-        usernameField: 'name'
+      usernameField: 'name'
     },
     (name, password, done) => {
-        User.findOne({'name': name}, (err, user) => {
-            if (err) return done(err);
-            if (!user) {
-                return done(null, false, { message: 'Incorrect Username'})
-            }
-            if (user.password != password) {
-                return done(null, false, {message: 'Incorrect Password'});
-            }
-            return done(null, user);
-        });
+      User.findOne({ name: name }, (err, user) => {
+        if (err) return done(err);
+        if (!user) {
+          return done(null, false, { message: 'Incorrect Username' });
+        }
+        if (user.password != password) {
+          return done(null, false, { message: 'Incorrect Password' });
+        }
+        return done(null, user);
+      });
     }
-));
+  )
+);
 
-passport.use(new GoogleStrategy(
+passport.use(
+  new GoogleStrategy(
     {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK
     },
-function (accessToken, refreshToken, profile, cb) {
-    User.findOne({'googleId': profile.id}, (err, user) => {
+    function(accessToken, refreshToken, profile, cb) {
+      User.findOne({ googleId: profile.id }, (err, user) => {
         if (err) return cb(err);
         if (user) {
+          if (!user.avatar) {
+            user.avatar = profile.photos[0].value;
+            user.save(err => {
+              return cb(null, user);
+            });
+          } else {
             return cb(null, user);
+          }
         } else {
-            // here we should have a NEW user via OAuth!
-            let newUser = new User({
-                name: profile.displayName,
-                email: profile.emails[0].value,
-                googleId: profile.id
-            });
-            newUser.save(err => {
-                if (err) return cb(err);
-                return cb(null, newUser);
-            });
+          // here we should have a NEW user via OAuth!
+          let newUser = new User({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id
+          });
+          newUser.save(err => {
+            if (err) return cb(err);
+            return cb(null, newUser);
+          });
         }
-    })
-}
-));
+      });
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-        done(err, user);
-    });
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
 });
--- controllers/admins.js
+++ controllers/admins.js
@@ -1,29 +1,59 @@
 const Service = require('../models/service');
 
-
 module.exports = {
-    index,
-    services
-}
+  index,
+  services,
+  tasks,
+  appts,
+  revenue
+};
 
 function index(req, res, next) {
-    Service.find({}, (err, services) => {
-        res.render('admin/index', {
-            title: 'Dashboard | Service App',
-            heading: 'Dashboard',
-            services
-
-        });
-    })
-   
+  Service.find({}, (err, services) => {
+    res.render('admin/index', {
+      title: 'Dashboard | Service App',
+      heading: 'Dashboard',
+      services
+    });
+  });
 }
 
 function services(req, res, next) {
-    Service.find({}, (err, services) => {
-        res.render('admin/services/index', {
-            title: 'Dashboard | Services',
-            heading: 'Services',
-            services
-        });
-    })
+  Service.find({}, (err, services) => {
+    res.render('admin/services/index', {
+      title: 'Dashboard | Services',
+      heading: 'Services',
+      services
+    });
+  });
+}
+
+function appts(req, res, next) {
+  Service.find({}, (err, services) => {
+    res.render('admin/appts/index', {
+      title: 'Dashboard | Services',
+      heading: 'Appointments',
+      services
+    });
+  });
+}
+
+function tasks(req, res, next) {
+  Service.find({}, (err, services) => {
+    res.render('admin/services/index', {
+      title: 'Dashboard | Services',
+      heading: 'Services',
+      services
+    });
+  });
+}
+
+function revenue(req, res, next) {
+  Service.find({}, (err, services) => {
+    res.render('admin/services/index', {
+      title: 'Dashboard | Services',
+      heading: 'Services',
+      services
+    });
+  });
 }
